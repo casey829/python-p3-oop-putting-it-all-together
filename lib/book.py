7840264299d631ffc837a000d9bf5cf73a0a5c66
casey <caseymwangi31@gmail.com>
2024-05-29 17:54:13 +0300

    putting it all together

--- lib/book.py
+++ lib/book.py
@@ -4,10 +4,11 @@ class Book:
     def __init__(self, title, page_count):
         self.title = title
         self.page_count = page_count
-    
+        
     @property
     def page_count(self):
         return self._page_count
+    
     
     @page_count.setter
     def page_count(self, value):
@@ -15,11 +16,8 @@ class Book:
             self._page_count = value
         else:
             print("page_count must be an integer")
-
-
+    
     def turn_page(self):
         print("Flipping the page...wow, you read fast!")
-    
-
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -4,21 +4,18 @@ class Shoe:
     def __init__(self, brand, size):
         self.brand = brand
         self.size = size
-        self.condition = "New"
-
-
+        self.condition = "condition"
+        
     @property
     def size(self):
         return self._size
     
-
     @size.setter
     def size(self, value):
         if isinstance(value, int):
             self._size = value
         else:
             print("size must be an integer")
-
     def cobble(self):
         self.condition = "New"
         print("Your shoe is as good as new!")
